5874ba59d9f786c793ee6d9739d1ef222719d455
Ruslan <ruslanmirzaev16621@gmail.com>
2020-11-19 11:06:13 +0700

    Промежуточная версия

--- First.cpp
+++ First.cpp
@@ -1,8 +1,15 @@
 #include <iostream>
-
+using namespace std;
 
 int main()
 {
-   
+   int result;    
+   int n,x;
+   cin >> n;
+   cin >> x;
+   result = x;
+   for(int i = 1; i < n ; i++){
+           result*=x;
+   }
+    cout << result;
 }
-
