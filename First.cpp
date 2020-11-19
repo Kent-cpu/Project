02a746e52e7f8a3e407efb6b3258ae62c8c0f104
Ruslan <ruslanmirzaev16621@gmail.com>
2020-11-19 11:13:52 +0700

    Рефакторинг

--- First.cpp
+++ First.cpp
@@ -3,12 +3,12 @@ using namespace std;
 
 int main()
 {
-   int result;    
-   int n,x;
-   cin >> n;
-   cin >> x;
+   int result = 0;    
+   int n = 0;
+   int x = 0;
+   cin >> n >> x;
    result = x;
-   for(int i = 1; i < n ; i++){
+   for(int i = 1; i < n ; ++i){
            result*=x;
    }
     cout << result;
